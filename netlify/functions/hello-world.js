9dd97ac322fc3aebfe19012c6446530693be5d7b
erezrokah <erezrokah@users.noreply.github.com>
2022-01-03 15:41:33 +0100

    chore: add example

---
+++ netlify/functions/hello-world.js
@@ -0,0 +1,13 @@
+const handler = async (event) => {
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
